b29f829370c38f2c4428bcc8f05b422b3aac0754
Josh Ayres <me@joshayres.tech>
2023-06-16 00:36:55 -0700

    ls improvements

--- prep/c/ls.c
+++ prep/c/ls.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -14,55 +15,73 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-int main(int argc, char **argv)
+typedef enum
 {
-    char *buf = malloc(1024);
-    size_t size = 1024;
-    const char *cwd = getcwd(buf, size);
+    NONE = 0,
+    ALL = 1,
+    LONG = 2,
+    SIZE = 4,
+    LIST = 8,
+}flags;
 
-    char *path = malloc(1024);
-    strcpy(path, cwd);
-    // parse flags here
-    if(argc > 1)
-    {
-        sprintf(path, "%s/%s", cwd, argv[argc-1]);
-    }
-
+int list(const char* path, flags flag)
+{
     DIR *dir = opendir(path);
 
     struct dirent *info = readdir(dir);
     while(info != NULL)
     {
+        struct stat stat_buf;
+        stat(info->d_name, &stat_buf);
         unsigned char *type;
-        switch(info->d_type){
-        case DT_BLK:
+        switch(stat_buf.st_mode & S_IFMT){
+        case S_IFBLK:
             type = ANSI_COLOR_GREEN;
             break;
-        case DT_CHR:
+        case S_IFCHR:
             type = ANSI_COLOR_RED;
             break;
-        case DT_DIR:
+        case S_IFDIR:
             type = ANSI_COLOR_CYAN;
             break;
-        case DT_FIFO:
+        case S_IFIFO:
             break;
-        case DT_LNK:
+        case S_IFLNK:
             type = ANSI_COLOR_YELLOW;
             break;
-        case DT_REG:
+        case S_IFREG:
             type = ANSI_COLOR_BLUE;
             break;
-        case DT_SOCK:
+        case S_IFSOCK:
             type = ANSI_COLOR_MAGENTA;
             break;
         default:
-            type = "";
-            break;
+            type = ""; break;
         }
-        printf("%s%s " ANSI_COLOR_RESET, type, info->d_name);
+        printf("%s%s %d " ANSI_COLOR_RESET, type, info->d_name, stat_buf.st_size);
         info = readdir(dir);
     }
     printf("\n");
     
+    closedir(dir);
+}
+
+
+int main(int argc, char **argv)
+{
+    for(int i = 1; i < argc; i++)
+    {
+        if(argv[i][0] == '-')
+        {
+            // TODO: flag
+            continue;
+        }
+        list(argv[i], NONE);
+    }
+    if(argc == 1)
+    {
+        list(".", NONE);
+    }
+
     return 0;
 }
